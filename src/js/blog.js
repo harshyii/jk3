64b3d656e261f4c819b69c232b0cf7f48084815d
Gala <makeagoodfriend@gmail.com>
2026-07-22 21:26:06 +0530

    major changes to ui

--- src/js/blog.js
+++ src/js/blog.js
@@ -74,9 +74,11 @@ document.addEventListener('DOMContentLoaded', async () => {
 
             // Rebuild container structure to host grid and pagination footer cleanly
             container.innerHTML = `
-                <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4" id="blog-grid"></div>
-                <div id="pagination-placeholder" class="d-flex justify-content-center mt-5"></div>
-            `;
+        <div class="col-12">
+            <div class="row row-cols-1 row-cols-md-2 row-cols-xl-3 g-4" id="blog-grid"></div>
+            <div id="pagination-placeholder" class="d-flex justify-content-center mt-5"></div>
+        </div>
+    `;
 
             const grid = document.getElementById('blog-grid');
             const paginationPlaceholder = document.getElementById('pagination-placeholder');
